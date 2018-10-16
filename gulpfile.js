d0015ed6b79fe57df57ee978c9a2046204718f25
Aykol Koykelov <kolyakoikelov@gmail.com>
2018-10-16 13:24:19 +0600

    + added gulp settings

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+var minifycss = require('gulp-minify-css');
+var stylus = require('gulp-stylus');
+
+gulp.task('images', function(){
+  gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('staticfiles/dist/images/'));
+});
+
+gulp.task('styles', function(){
+  gulp.src(['src/styles/**/*.styl'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(stylus())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('dist/styles/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('staticfiles/dist/styles/'))
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('staticfiles/dist/scripts/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('staticfiles/dist/scripts/'))
+});
+
+gulp.task('default', function(){
+  gulp.watch("src/styles/**/*.styl", ['styles']);
+  gulp.watch("src/scripts/**/*.js", ['scripts']);
+});
