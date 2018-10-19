2fa92270ca644642ed506cd8cc0e576b3ae9a013
rasul312 <freeman190793@gmail.com>
2018-10-19 15:03:50 +0600

    fix gulpjs and start js css

--- gulpfile.js
+++ gulpfile.js
@@ -9,42 +9,48 @@ var imagemin = require('gulp-imagemin'),
 var minifycss = require('gulp-minify-css');
 var stylus = require('gulp-stylus');
 
-gulp.task('images', function(){
-  gulp.src('src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('staticfiles/dist/images/'));
+gulp.task('images', function () {
+    gulp.src('src/images/**/*')
+        .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
+        .pipe(gulp.dest('staticfiles/dist/images/'));
 });
 
-gulp.task('styles', function(){
-  gulp.src(['src/styles/**/*.styl'])
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(stylus())
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('dist/styles/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('staticfiles/dist/styles/'))
+gulp.task('styles', function () {
+    gulp.src(['src/styles/main.styl'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(stylus({
+            'include css': true,
+            compress: true,
+            linenos: true
+        }))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('dist/styles/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('staticfiles/dist/styles/'))
 });
 
-gulp.task('scripts', function(){
-  return gulp.src('src/scripts/**/*.js')
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('staticfiles/dist/scripts/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('staticfiles/dist/scripts/'))
+gulp.task('scripts', function () {
+    return gulp.src(['src/scripts/libs/jQuery_v3.3.1.js', 'src/scripts/libs/uikit.js', 'src/scripts/libs/uikit-icons.js', 'src/scripts/back.js', 'src/scripts/front.js'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('staticfiles/dist/scripts/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('staticfiles/dist/scripts/'))
 });
 
-gulp.task('default', function(){
-  gulp.watch("src/styles/**/*.styl", ['styles']);
-  gulp.watch("src/scripts/**/*.js", ['scripts']);
+gulp.task('default', function () {
+    gulp.watch("src/styles/**/*.styl", ['styles']);
+    gulp.watch("src/scripts/**/*.js", ['scripts']);
 });
