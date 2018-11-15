a41ec22235c19b0b5f4e55870b3b5320a9eec073
admin2 <admin2@MacBook-Pro-Vladimir.local>
2018-11-15 18:06:41 +0600

    ^ changed start.sh script

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 virtualenv venv --python=python3
+curl https://bootstrap.pypa.io/get-pip.py | venv/bin/python
 venv/bin/pip install -r requirements/base.txt
 venv/bin/python ./manage.py migrate
 
