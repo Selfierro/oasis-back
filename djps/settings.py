e2bc033f1407967a4f507072d0e494a2fbc685ab
Aykol Koykelov <kolyakoikelov@gmail.com>
2019-03-15 19:06:22 +0600

    + added celery lib

--- djps/settings.py
+++ djps/settings.py
@@ -84,13 +84,17 @@ WSGI_APPLICATION = 'djps.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': '',
+    #     'USER': '',
+    #     'PASSWORD': '',
+    #     'HOST': 'postgres',
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'postgres',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
